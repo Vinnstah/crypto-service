bbdd253b0498297c09a224821d9849ed76593065
Viktor Jansson <v.l.jansson@gmail.com>
2024-02-21 21:51:42 +0100

    Missing arg labels

--- apple/Sources/CryptoService/CryptoService.swift
+++ apple/Sources/CryptoService/CryptoService.swift
@@ -4,10 +4,10 @@ public struct Client {}
 
 extension Client {
     public func getSymbols(params: SymbolsParams) async -> [SymbolsResponse]  {
-      getSymbolsBinding(params) 
+      getSymbolsBinding(params: params) 
     }
 
     public func getOrderbook(params: Params) -> OrderBookResponse {
-      getOrderbookBinding(params) 
+      getOrderbookBinding(params: params) 
     }
 }
