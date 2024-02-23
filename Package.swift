92f1f8f63c5f0c9f30099470e0514e1b26e463b6
Anka <runner@Mac-1708677705493.local>
2024-02-23 09:11:52 +0000

    Release of 'v0.1.2' (updated Package.swift with new checksum and path to zip on Github, and maybe apple/Sources/UniFFI/CryptoService.swift). This commit is not merged into main/develop branch (and need not be).

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ if useLocalFramework {
 		path: "./target/ios/libcrypto_service-rs.xcframework"
 	)
 } else {
-	let releaseTag = "0.1.0"
-	let releaseChecksum = "befef7d56108305ff6ff69d67483471395c3e603e299b3b15f5a826328de272b"
+	let releaseTag = "v0.1.2"
+	let releaseChecksum = "18a36db0b059805238845e4827b548ef29ebdd47140a43532192456339e4bec2"
 	binaryTarget = .binaryTarget(
 		name: cryptoServiceBinaryTargetName,
 		url:
