6bdcb16cf4ea61e03e3a89c2bbab2cf8b83a707c
Viktor Jansson <v.l.jansson@gmail.com>
2024-02-29 13:21:30 +0100

    WIP

--- crates/crypto-service-uniffi/src/api_client/api_client.rs
+++ crates/crypto-service-uniffi/src/api_client/api_client.rs
@@ -1,24 +1,71 @@
 use std::{collections::HashMap, sync::Arc};
 
 use anyhow::Error;
+use axum::Json;
 use reqwest::header::HeaderMap;
+use serde::Serialize;
+use serde_json::Serializer;
 use uniffi::{check_remaining, metadata, Lift, Lower, LowerReturn, MetadataBuffer, RustBuffer};
 
-use crate::client_trait::QueryItems;
+use crate::{
+    binance_service::models::OrderBook, client_trait::QueryItems,
+    coinapi_service::models::SymbolsResponse,
+};
 
+/// protocol NetworkManager {
+///     func install(self) -> some RequestHandler   
+/// }
+///
+/// struct Coordinator: NetworkManager {
+///     func install(self) -> some RequestHandler {
+///         ApiClient(client: .....)
+///
 
-pub struct ApiClient {
-    client: Arc<reqwest::Client>,
+#[uniffi::export(callback_interface)]
+pub trait NetworkAntenna: Send + Sync {
+    fn send(&self, request: RequestCases) -> Result<Response, DataTaskFailure>;
 }
 
-impl ApiClient {
-    #[uniffi::constructor]
-    pub fn new() -> Self {
-        Self {
-            client: reqwest::Client::new().into(),
-        }
-    }
+#[derive(uniffi::Enum)]
+pub enum RequestCases {
+    Orderbook { orderbook: OrderBook },
+    Symbols { symbolls: SymbolsResponse },
 }
+
+#[derive(uniffi::Enum)]
+pub enum Response {
+    Orderbook { orderbook: OrderBook },
+    Symbols { symbolls: SymbolsResponse },
+}
+
+pub fn request(
+    network_antenna: Box<dyn NetworkAntenna>,
+    request: RequestCases,
+) -> Result<Response, DataTaskFailure> {
+
+    network_antenna.send(request)
+}
+
+// #[derive(uniffi::Record)]
+// pub struct ApiClient {
+//     client: Arc<dyn RequestHandler>,
+// }
+
+// impl ApiClient {
+//     #[uniffi::constructor]
+//     pub fn new(data_task: Arc<dyn RequestHandler>) -> Arc<Self> {
+//         Arc::new(Self { client: data_task })
+//     }
+// }
+
+#[derive(uniffi::Error)]
+pub enum DataTaskFailure {}
+
+#[derive(uniffi::Record)]
+pub struct Data {}
+
+#[derive(uniffi::Record)]
+pub struct URLRequest {}
 
 // #[uniffi::export]
 // impl ApiClient {
