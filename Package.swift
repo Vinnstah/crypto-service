fa0d94e936174b2a118efe92b4afb5fc003df4b7
Viktor Jansson <v.l.jansson@gmail.com>
2024-03-25 22:29:07 +0100

    Set LocalFramework false

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let cryptoServiceBinaryTargetName = "CryptoServiceRS"
 let binaryTarget: Target
-let useLocalFramework = true
+let useLocalFramework = false
 
 if useLocalFramework {
 	binaryTarget = .binaryTarget(
